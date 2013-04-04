5bfd2df87825d8e5edd44fb66a5f448b4c946715
Donald Clark <Donald@67-194-113-52.wireless.umnet.umich.edu>
2013-04-03 22:25:27 -0400

    almost done

---
+++ eecs381_project5/Views.cpp
@@ -0,0 +1,9 @@
+//
+//  Views.cpp
+//  eecs381_project5
+//
+//  Created by Donald Clark on 4/3/13.
+//  Copyright (c) 2013 Donald Clark. All rights reserved.
+//
+
+#include "Views.h"
---
+++ eecs381_project5/Views.h
@@ -0,0 +1,14 @@
+//
+//  Views.h
+//  eecs381_project5
+//
+//  Created by Donald Clark on 4/3/13.
+//  Copyright (c) 2013 Donald Clark. All rights reserved.
+//
+
+#ifndef __eecs381_project5__Views__
+#define __eecs381_project5__Views__
+
+#include <iostream>
+
+#endif /* defined(__eecs381_project5__Views__) */
